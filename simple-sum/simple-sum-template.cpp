c2cb02f49d75e6e80f64c33e42d6eb0439d7ee3d
Andrea Prati <a.prati@outlook.it>
2024-01-12 18:59:45 +0100

    implementing generics and playing with random double

---
+++ simple-sum/simple-sum-template.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <ctime>
+
+
+using namespace std;
+
+#define N 40
+
+template <typename T>
+
+void sum(T &res, int size, T arr[]) 
+{
+
+    for (int i = 0; i < size; i++)
+    {
+        res += arr[i];
+    }
+    
+}
+
+int main() 
+{
+    double res = 0;
+    double arr[N];
+    
+    srand(clock()); // setting the seed for the random function
+
+
+    for (int i = 0; i < N; i++)
+    {
+        arr[i] = rand()/static_cast<double>(rand());
+    }
+    
+    sum(res, N, arr);
+
+    cout << "The result is: " << res << endl;
+    cout << clock() << endl;
+
+    return 0;
+}
