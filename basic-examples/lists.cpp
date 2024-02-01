e4a15f6af99c6f789879e81bc7399cbb092ff466
Andrea Prati <a.prati@outlook.it>
2024-02-01 16:52:10 +0100

    created List class and implemented prepend method

--- basic-examples/lists.cpp
+++ basic-examples/lists.cpp
@@ -9,7 +9,34 @@ class List_element
         List_element(int n = 0, List_element* next = nullptr): data(n), nextElement(next) {}
 };
 
-int main() 
+class List
 {
+    public:
+        List(): head(nullptr), cursor(nullptr) {}
+        
+        void prepend(int n)
+        {
+            // this case, the list is empty, the head points at nothing
+            if(head == nullptr) {
+                cursor = head = new List_element(n, head);
+            } else {
+                head = new List_element(n, head);
+            }
+        }
+
+        int get_element() {return cursor -> data;}
+
+        void advance() {cursor = cursor -> nextElement;}
+        
+        void print();
+        
+    private: 
+        List_element* head;
+        List_element* cursor;
+};
+
+
+
+int main() {
     return 0;
 }
