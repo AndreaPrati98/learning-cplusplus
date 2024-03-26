48bb52f115db72492d37ae858517eeffe9dca099
Andrea Prati <a.prati@outlook.it>
2024-03-26 15:14:04 +0100

    ifstream used to read words and did to lower conversion over the string using iterator

--- basic-examples/filestream.cpp
+++ basic-examples/filestream.cpp
@@ -2,25 +2,47 @@
 #include <iterator>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    ifstream data_file("./files/data.txt");
-    istream_iterator<int> start(data_file), end;
+    ifstream word_file("./files/data.txt");
+    istream_iterator<string> start(word_file), end;
 
     // we are now pointing at the beginning of the file.
-    vector<int> data(start, end);
+    vector<string> words(start, end);
 
-    int sum = 0;
-    for (auto it = start; it != end; ++it) {
-        sum += *it;
+    // automatically each white space set of chars makes the delimiter
+
+    cout << " \n\nwords as read\n" ;
+    for (const auto &str : words)
+    {
+        cout << str << "\t";
     }
-
-    cout << "Sum = " << sum << endl;
-    cout << "average is = " << 1.0 * sum/data.size() << endl;
     
+    
+    cout << " \n\nwords to lowercase\n" ;
+    for (auto &str : words)
+    {
+        // here I use the transform method in order to cycle over the string str and convert every char to lowercase
+        // notice I use the pass-by-reference, otherwise the conversion wouldn't work.   
+        transform(str.begin(), str.end(), str.begin(), [](unsigned char c) { return tolower(c); } );
+        
+        cout << str << "\t";
+    }
+    cout << endl;
+
+
+
+    sort(words.begin(), words.end());
+    cout << " \n\nwords as read\n" ;
+    for (auto str : words)
+    {
+        cout << str << "\t";
+    }
+    cout << endl;
 
     return 0;
 }
