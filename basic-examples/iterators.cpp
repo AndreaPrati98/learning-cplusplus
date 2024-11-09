9ca6f0e9fb0e3a026f91821be2c220a334b28a3d
Andrea Prati <a.prati@outlook.it>
2024-11-09 22:53:08 +0100

    Little improvement

--- basic-examples/iterators.cpp
+++ basic-examples/iterators.cpp
@@ -2,20 +2,29 @@
 
 using namespace std;
 
+void without_iterator();
+
 int main() {
-    
+
+    without_iterator();
+
+    return 0;
+}
+
+void without_iterator() {
     vector<unsigned> scores(11,0);
 
     unsigned grade;
     while (cin >> grade) {
-        if(grade < 100)
+        if(grade <= 100)
             ++scores[grade/10];
+        else
+            cout << "Ignoring input" << endl;
     }
 
     for(const auto &el : scores) {
         cout << el << " ";
     }
-    cout << endl;
 
-    return 0;
+    cout << endl;
 }
