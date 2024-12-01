cc107ad13b6374c738b71c62e65f5378b7da324c
Andrea Prati <a.prati@outlook.it>
2024-12-01 12:47:54 +0100

    solution

--- leetCodes/27removeElement.cpp
+++ leetCodes/27removeElement.cpp
@@ -11,39 +11,16 @@ public:
             return nums.size();
         }
 
-        int k = 0, r = nums.size() - 1;
-        while (k < nums.size() && r != k) 
+        int k = 0, r = 0;
+        while ( r < nums.size() ) 
         {
-            
-            if(nums[k] == val) 
-            {
-                // swapping procedure
-                while (nums[r] == val && r > k)
-                {
-                    // looking in the right pointer an elem
-                    // different than val
-                    --r;
-                }
-                    
-                // found element different than val
-                // and it is not already taken into
-                // account by k
-                
-                int tmp = nums[r];
-                nums[r] = nums[k];
-                nums[k] = tmp;
-                --r;
-
-            }
-
-            if(nums[k] != val) 
-            {
-                // it may be incremented if we find a nums[k] != val
-                // or if we find that nums[r] != val and r != k;
+            if ( nums[r] != val ) {
+                swap(nums[k], nums[r]);
                 ++k;
-            }   
-
-        } 
+            }
+            ++r;
+        }
         return k;
     }
 };
+
