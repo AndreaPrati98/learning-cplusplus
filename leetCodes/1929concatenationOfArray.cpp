17b9218f491e90201c38c6e61dadb6fccd6dd6a9
Andrea Prati <a.prati@outlook.it>
2024-12-01 18:38:22 +0100

    better

--- leetCodes/1929concatenationOfArray.cpp
+++ leetCodes/1929concatenationOfArray.cpp
@@ -5,16 +5,13 @@ using namespace std;
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        auto newCapacity = nums.size();
-        vector<int> ans = vector<int>(newCapacity * 2);
-        for(size_t i = 0; i < ans.size();)  
-        {
-            for (size_t j = 0; j < nums.size(); j++)
-            {
-                ans[i] = nums[j];
-                ++i;
-            }
-            
+        auto oldSize = nums.size();
+        vector<int> ans = vector<int>(oldSize * 2);
+        size_t j = oldSize; // starting j in the middle
+        for (size_t i = 0; i < nums.size(); ++i) {
+            ans[i] = nums[i];
+            ans[j] = nums[i];
+            ++j;
         }
         return ans;
     }
