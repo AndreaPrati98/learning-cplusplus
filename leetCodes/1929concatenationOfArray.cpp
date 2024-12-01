e01708e9a776650b038277d92809c159fa9b460d
Andrea Prati <a.prati@outlook.it>
2024-12-01 18:27:36 +0100

    size and capacity are not the same thing, capacity can be larger than size

--- leetCodes/1929concatenationOfArray.cpp
+++ leetCodes/1929concatenationOfArray.cpp
@@ -5,11 +5,11 @@ using namespace std;
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        auto newCapacity = nums.capacity();
+        auto newCapacity = nums.size();
         vector<int> ans = vector<int>(newCapacity * 2);
-        for(size_t i = 0; i < ans.capacity();)  
+        for(size_t i = 0; i < ans.size();)  
         {
-            for (size_t j = 0; j < nums.capacity(); j++)
+            for (size_t j = 0; j < nums.size(); j++)
             {
                 ans[i] = nums[j];
                 ++i;
