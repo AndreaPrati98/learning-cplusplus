70287d5587ac4e65e82bf80a66ee4d42eece8f0d
Andrea Prati <a.prati@outlook.it>
2024-12-07 14:46:06 +0100

    it doesn't work

--- leetCodes/21mergeTwoSortedLinkedLists.cpp
+++ leetCodes/21mergeTwoSortedLinkedLists.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+
 struct ListNode {
     int val;
     ListNode *next;
@@ -10,6 +11,7 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
  
+void test1(ListNode* &, ListNode* &);
 
 class Solution {
 public:
@@ -26,19 +28,34 @@ public:
         
         while(first && second) 
         {
+            cout << "First: "<< first << " val: " << first -> val <<"\tSecond: " << second << " val: " << second -> val << endl;
             if((first -> val) < (second -> val))
             {
                 interlaceNodes(first, second);
-            } else {
-                interlaceNodes(second, first);
-            }
-            cout << "First: "<< first <<"\tSecond: " << second << endl;
-            
+            } 
+            else 
+            {
+                bool equalValues = first -> val == second -> val;
+                bool noNullptrs = first -> next != nullptr && second -> next != nullptr;
+
+                if (equalValues && noNullptrs)
+                {
+                    cout << "here" << endl;
+                    if((first -> next -> val) <= (second -> next -> val)) {
+                        interlaceNodes(first, second);
+                    } else {
+                        interlaceNodes(second, first);
+                    }
+                } else {
+                    cout << "Now here" << endl;
+                    interlaceNodes(second, first);
+                }
+            }   
         }
 
         // need to understand what is pointing what
         // to figure out the first node
-        if(!list1 && (list1 -> next == list2))
+        if((list1 != nullptr) && (list1 -> next == list2))
             return list1;
         else
             return list2;
@@ -61,18 +78,27 @@ public:
 
 
 int main() {
-    ListNode *e = new ListNode(5);
-    ListNode *c = new ListNode(3, e);
-    ListNode *a = new ListNode(1, c);
+    ListNode *list1;
+    ListNode *list2;
 
-    ListNode *d = new ListNode(4);
-    ListNode *b = new ListNode(2, d);
+    test1(list1, list2);
 
     Solution s = Solution();
-    s.debug(a);
-    s.debug(b);
-    ListNode * res = s.mergeTwoLists(a, b);
+    s.debug(list1);
+    s.debug(list2);
+    ListNode * res = s.mergeTwoLists(list1, list2);
     s.debug(res);
 
     return 0;
-};
+}
+
+void test1(ListNode* &list1, ListNode* &list2) {
+    ListNode *e = new ListNode(4);
+    ListNode *c = new ListNode(2, e);
+    list1 = new ListNode(1, c);
+    
+
+    ListNode *f = new ListNode(4);
+    ListNode *d = new ListNode(3, f);
+    list2 = new ListNode(1, d);
+}
