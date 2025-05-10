2836e2791fbb56141ef8239bca4bb77214ac2426
Andrea Prati <a.prati@outlook.it>
2025-05-10 23:54:39 +0200

    cleanup

---
+++ DataStructuresFromScratch/DoubleEndedQueue.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode{
+    int val;
+    ListNode* next;
+
+    ListNode() : ListNode(0, nullptr) {};
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+}
+
+class Deque {
+public:
+    Deque() {
+        
+    }
+
+    bool isEmpty() {
+
+    }
+
+    void append(int value) {
+        
+    }
+
+    void appendleft(int value) {
+        
+    }
+
+    int pop() {
+        
+    }
+
+    int popleft() {
+
+    }
+};
+
+
+int main() {
+    return 0;
+}
